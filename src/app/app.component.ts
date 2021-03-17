c87ade338e4efcb484dd8f819b02297e26fe76db
Andrey <cattalin.htc@gmail.com>
2021-03-17 12:57:16 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGlobeEurope, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mini-bio';
+  moon = faMoon
+  sun = faSun
+  isNightMode = false
+
+  socialNetworks = [
+    {
+      link: "http://github.com/andreycattalin",
+      image: faGithub,
+      title: "Mis proyectos de github"
+    },
+    {
+      link: "https://twitter.com/andreycattalin",
+      image: faTwitter,
+      title: "Este es mi twitter"
+    },
+    {
+      link: "https://attomic.es",
+      image: faGlobeEurope,
+      title: "La web de attomic"
+    },
+    {
+      link: "https://www.linkedin.com/in/andreymarin",
+      image: faLinkedin,
+      title: "Mi linkedin"
+    }
+  ]
+
+  constructor(private renderer: Renderer2) {
+
+  }
+
+  nightMode() {
+    this.isNightMode = !this.isNightMode
+    if(this.isNightMode) {
+      this.renderer.addClass(document.body, 'night');
+    } else {
+      this.renderer.removeClass(document.body, 'night');
+    }
+  }
+
 }
